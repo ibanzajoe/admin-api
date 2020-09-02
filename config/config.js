b565c3f25be350f58043d368ddeacb9c1dff7739
Dennis <ibanzajoe@gmail.com>
2020-09-01 20:58:30 -0700

    added ssl-required

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   port: process.env.PORT || 8081,
   db: {
-    dsn: 'postgresql://doadmin:kkaz0ar60xdk14j3@nuxt-admin-db-do-user-1558822-0.a.db.ondigitalocean.com:25060/defaultdb'
+    dsn: 'postgresql://doadmin:kkaz0ar60xdk14j3@nuxt-admin-db-do-user-1558822-0.a.db.ondigitalocean.com:25060/defaultdb?sslmode=require&ssl=true'
   },
   authentication: {
     jwtSecret: process.env.JWT_SECRET || 'li#223GX3$2'
